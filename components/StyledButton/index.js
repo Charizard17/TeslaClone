2e3e9b45e8a9d1b6e6376d94b94a9d1533d8fa6e
Esad Dursun <3723esad@gmail.com>
2021-10-12 21:51:25 +0200

    button component is done

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -3,15 +3,23 @@ import {View, Text, Pressable} from 'react-native';
 import styles from './styles';
 
 const CarItem = (props) => {
+
+    // const type = props.type;
+    // const content = props.content;
+    // const onPress = props.onPress;
+
+    const { type, content, onPress } = props;
+    
+    const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
+    const textColor = type === 'primary' ? '#FFFFFF' : '#171A20CC';
+
     return (
         <View style={styles.container}>
             <Pressable 
-                style={styles.button}
-                onPress={() => {
-                    console.warn('Hey there!')
-                }}
+                style={[styles.button, {backgroundColor: backgroundColor}]}
+                onPress={() => onPress()}
             >
-                <Text style={styles.text}>Custom Order</Text>
+                <Text style={[styles.text, {color: textColor}]}>{content}</Text>
             </Pressable>
         </View>
     )
